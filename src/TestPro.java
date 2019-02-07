9b6dcbf665c3b8e73d657aeef4d1c0e9bd26b3ee
VivekPro <iamvivekpro@gmail.com>
2019-02-07 15:39:13 +0530

    Instance of Confusion

---
+++ src/TestPro.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class A{
+	int a;
+	public A() {}
+}
+class B extends A{
+	int b;
+	public B() {
+		
+		
+	}
+}
+class C{
+	
+	C(){}
+}
+public class TestPro {
+	public static void main(String []a) {
+	A obj1=new A();
+	B obj=new B();
+	C obs=new C();
+	Object ob2=new Object();
+	System.out.println(obj1 instanceof Object);
+	}
+}
