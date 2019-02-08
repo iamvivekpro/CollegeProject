7e4d2066bf79f84d5096b85ad69e17d318164cf5
VivekPro <iamvivekpro@gmail.com>
2019-02-08 22:08:01 +0530

    SingleTon Pattern Demo Finished

--- src/singalton_pattern/Singelton.java
+++ src/singalton_pattern/Singelton.java
@@ -1,9 +1,20 @@
 package singalton_pattern;
 
-public class Singelton {
-
-	public Singelton() {
+class Singelton {
+	static Singelton s;
+	private Singelton() {
+		s=new Singelton();
 		// TODO Auto-generated constructor stub
 	}
+	public static  Singelton getInstanceSingelton() {
+		return s;
+		
+	}
+	public static void display() {
+		
+		System.out.println("Hello Display");
+	}
+	
 
 }
+
---
+++ src/singalton_pattern/SingleTonMain.java
@@ -0,0 +1,10 @@
+package singalton_pattern;
+
+public class SingleTonMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Singelton.getInstanceSingelton().display();
+	}
+
+}
