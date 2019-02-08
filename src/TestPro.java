a7b9d0cf8ff3b7e67ed7fe6862cb802ba92fe700
VivekPro <iamvivekpro@gmail.com>
2019-02-08 21:19:14 +0530

    Pro111

---
+++ src/TestPro.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class A{
+	/*
+	 * int a; public A() {}
+	 */
+}
+class B extends A{
+	int b;
+	public B() {
+		System.out.println("Hello World");
+	}
+}
+class C{
+	C(){
+		System.out.println("Hello World");
+	}
+}
+public class TestPro {
+	public static void main(String []a) {
+	A obj1=new A();
+	B obj=new B();
+	C obs=new C();
+	Object ob2=new Object();   						//Hello World
+	System.out.println(obj1 instanceof Object);		//Hello World
+	
+	System.out.println("hello New Branch");
+	
+	}
+}
