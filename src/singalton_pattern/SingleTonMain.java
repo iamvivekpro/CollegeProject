a4dc172367a018abd2f57e2761c26a286cd47e0c
VivekPro <iamvivekpro@gmail.com>
2019-02-11 11:37:16 +0530

    scv

--- src/singalton_pattern/SingleTonMain.java
+++ src/singalton_pattern/SingleTonMain.java
@@ -5,6 +5,7 @@ public class SingleTonMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Singelton s=Singelton.getInstanceSingelton();
+		
 		s.display();
 		System.out.println(s);
 	}
