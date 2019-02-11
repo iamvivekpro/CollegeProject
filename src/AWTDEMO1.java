6a63cb57567b524c51b7a216fc23a47a67314f68
VivekPro <iamvivekpro@gmail.com>
2019-02-11 20:56:25 +0530

    First Demo Button Maker

---
+++ src/AWTDEMO1.java
@@ -0,0 +1,8 @@
+
+public class AWTDEMO1 {
+
+	public AWTDEMO1() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/First.java
@@ -0,0 +1,18 @@
+import java.awt.*; 
+public class First extends Frame {
+
+	public First() {
+		// TODO Auto-generated constructor stub
+		Button b=new Button("click me");  
+		b.setBounds(30,100,80,30);// setting button position  
+		add(b);//adding button into frame  
+		setSize(300,300);//frame size 300 width and 300 height  
+		setLayout(null);//no layout manager  
+		setVisible(true);//now frame will be visible, by default not visible 
+	}
+	public static void main(String args[]){  
+		First f=new First();  
+		}
+
+}
+ 
