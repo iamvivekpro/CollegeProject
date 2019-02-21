8f08125ac1098624e44345540ac35d745c776f71
VivekPro <iamvivekpro@gmail.com>
2019-02-21 12:53:01 +0530

    Append Function Implementation

---
+++ src/StringLearning/AppendDemo.java
@@ -0,0 +1,37 @@
+package StringLearning;
+
+public class AppendDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 StringBuffer sb = new StringBuffer();
+         
+	        sb.append("Append String: ");
+	        String str = "JCG";
+	        sb.append('c');
+	        System.out.println("Now String is"+sb.toString());
+		/*
+		 * sb.append("n");
+		 * 
+		 * sb.append("Append boolean: "); boolean b = true; sb.append(b);
+		 * sb.append("n");
+		 * 
+		 * sb.append("Append char: "); char c = 'a'; sb.append(c); sb.append("n");
+		 * 
+		 * sb.append("Append char array: "); char[] ca = new char[] {'J','C','G'};
+		 * sb.append(ca); sb.append("n");
+		 * 
+		 * sb.append("Append double: "); double d = 1.0; sb.append(d); sb.append("n");
+		 * 
+		 * sb.append("Append float: "); float f = 1.0f; sb.append(f); sb.append("n");
+		 * 
+		 * sb.append("Append int: "); int i = 1; sb.append(i); sb.append("n");
+		 * 
+		 * sb.append("Append Object: "); Object obj = new String("JCG"); sb.append(obj);
+		 * sb.append("n");
+		 * 
+		 * System.out.println(sb.toString());
+		 */
+	    }
+}
